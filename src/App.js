22f8190165f8ed916f9cc75f9130f7cda439e2b2
phgodwin <paige227@hotmail.co.uk>
2023-11-28 12:43:59 +0000

    converting data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Title from './Components/Titles';
 import KeyMash from './Components/KeyMash';
 import Learners from './Components/Learners';
+import friends from './Data/friends.json';
+import MyFriend from './Components/MyFriend';
 
 
 function App() {
+// Log friends json component to the console
+  console.log("Friends:", friends);
+
+const friendComponents = [];
+// loop through your array (from json) and print each one to the console
+for (const friend of friends) {
+  console.log(friend)
+// convert the data into components
+  friendComponents.push(
+  <MyFriend name={friend.name} age={friend.age} hair={friend.hair} home={friend.home} />
+  );
+ 
+
+}
+console.log("components:", friendComponents)
   return (
-    <div>
-      <h1>I am React
+<div>
+    
+<h1>I am React
       </h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -33,9 +51,13 @@ function App() {
    <Learners name ="Eloise" age="24" job="Claims handler" color="Orange" />
    <Learners name ="Matthew" age="25" job="Civil servant" color="Black" />
 
+    <div>
+{friendComponents}
+      </div>
+    
 
       </div>
-      );
+    );
 }
 
  
---
+++ src/Components/MyFriend.jsx
@@ -0,0 +1,13 @@
+function MyFriend(props) {
+
+    return(
+    <div>
+        <p>Name: {props.name}</p>
+        <p>Age: {props.age}</p>
+        <p>Hair: {props.hair}</p>
+        <p>Home: {props.home} </p>
+    </div>)
+}
+
+export default MyFriend
+
